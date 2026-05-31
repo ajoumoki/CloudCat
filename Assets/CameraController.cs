18bb4257bd5f787fdb88f2814df13c41601280ae
R.M. <bda07-s250215@gclass.ice.go.kr>
2026-05-31 22:34:48 +0900

    Update CameraController.cs

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -14,7 +14,7 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 playerPos = this.player.transform.position;
+        Vector3 playerPos = this.player.transform.position; //playerPos값을 cat의 위치로 설정
         transform.position = new Vector3(
             transform.position.x,playerPos.y,transform.position.z);
     }
