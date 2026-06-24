fa31c2ec264b87394551b99738b514002adbf66e
R.M. <bda07-s250215@gclass.ice.go.kr>
2026-06-24 14:36:08 +0900

    Update CloudController.cs

--- Assets/CloudController.cs
+++ Assets/CloudController.cs
@@ -9,12 +9,12 @@ public class CloudController : MonoBehaviour
     {
         
     }
-    private void OnCollisionEnter2D(Collision2D collision  //이 게임오브젝트가 컬리션 충돌을 했을때
+    private void OnCollisionEnter2D(Collision2D collision  //이 게임오브젝트가 컬리션 충돌을 했을 때
     {
         print("구름 컬리션 충돌");
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision) //이 게임오브젝트가 트리거 충돌을 했을 때
     {
         print("구름 트리거 충돌");
     }
